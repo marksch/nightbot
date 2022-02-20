ad450c07e007ea4f2c691bb4be9998ad0025b7bf
marksch <m.schonewille@economy-x-talk.com>
2022-02-20 04:31:57 +0100

    cleaning up

--- api.php
+++ api.php
@@ -2,7 +2,6 @@
 
 ini_set('display_errors', 0);
 
-// include 'catch500.php';
 // the following because I always make the same mistake
 function random($x) {
 	return rand($x);
@@ -25,8 +24,9 @@ class parse {
 	public $pi = M_PI;
 	public $e = M_E;
 
-	// pretty much everymath function available
+	// pretty much every math function available in php
 	function math_func_list() {
+		// keeping the next line for reference
 		// $arr= array("e" => "exponential number", "pi" => "the constant pi", "abs" => "Returns the absolute (positive) value of a number", "acos" => "Returns the arc cosine of a number", "acosh" => "Returns the inverse hyperbolic cosine of a number", "asin" => "Returns the arc sine of a number", "asinh" => "Returns the inverse hyperbolic sine of a number", "atan" => "Returns the arc tangent of a number in radians", "atan2" => "Returns the arc tangent of two variables x and y", "atanh" => "Returns the inverse hyperbolic tangent of a number", "base_convert" => "Converts a number from one number base to another", "bindec" => "Converts a binary number to a decimal number", "ceil" => "Rounds a number up to the nearest integer", "cos" => "Returns the cosine of a number", "cosh" => "Returns the hyperbolic cosine of a number", "decbin" => "Converts a decimal number to a binary number", "dechex" => "Converts a decimal number to a hexadecimal number", "decoct" => "Converts a decimal number to an octal number", "deg2rad" => "Converts a degree value to a radian value", "exp" => "Calculates the exponent of e", "expm1" => "Returns exp(x) - 1", "floor" => "Rounds a number down to the nearest integer", "fmod" => "Returns the remainder of x/y", "getrandmax" => "Returns the largest possible value returned by rand", "hexdec" => "Converts a hexadecimal number to a decimal number", "hypot" => "Calculates the hypotenuse of a right-angle triangle", "intdiv" => "Performs integer division", "is_finite" => "Checks whether a value is finite or not", "is_infinite" => "Checks whether a value is infinite or not", "is_nan" => "Checks whether a value is 'not-a-number'", "lcg_value" => "Returns a pseudo random number in a range between 0 and 1", "log" => "Returns the natural logarithm of a number", "log10" => "Returns the base-10 logarithm of a number", "log1p" => "Returns log(1+number)", "max" => "Returns the highest value in an array, or the highest value of several specified values", "min" => "Returns the lowest value in an array, or the lowest value of several specified values", "mt_getrandmax" => "Returns the largest possible value returned by mt_rand", "mt_rand" => "Generates a random integer using Mersenne Twister algorithm", "mt_srand" => "Seeds the Mersenne Twister random number generator", "octdec" => "Converts an octal number to a decimal number", "pi" => "Returns the value of PI", "pow" => "Returns x raised to the power of y", "rad2deg" => "Converts a radian value to a degree value", "rand" => "Generates a random integer", "round" => "Rounds a floating-point number", "sin" => "Returns the sine of a number", "sinh" => "Returns the hyperbolic sine of a number", "sqrt" => "Returns the square root of a number", "srand" => "Seeds the random number generator", "tan" => "Returns the tangent of a number", "tanh" => "Returns the hyperbolic tangent of a number");
 		$arr = array('abs','acos','acosh','asin','asinh','atan','atan2','atanh','base_convert','bindec','ceil','cos','cosh','decbin','dechex','decoct','deg2rad','e','exp','expm1','floor','fmod','getrandmax','hexdec','hypot','intdiv','is_finite','is_infinite','is_nan','lcg_value','log','log10','log1p','max','min','mt_getrandmax','mt_rand','mt_srand','octdec','pi','pow','rad2deg','rand','random','round','sin','sinh','sqrt','srand','tan','tanh');
 		return $arr;
@@ -43,14 +43,11 @@ class parse {
 
 	function get_func_list($eq) {
 		// get all functions used in the equation (strings of letters and numbers) but not the symbols and the numbers
-		// $eq = "voorbeeld ^$";
 		$eq = $eq . '';
 		$eq = str_replace('(', ' ', $eq);
 		$eq = str_replace(')', ' ', $eq);
-		// the parantheses indicate what to replace
+		// the parentheses () indicate what to replace
 		$output = preg_replace(" ([0-9\-\=\/\+\*\^\,\.]+) ", ' ', $eq);
-		//$output = preg_replace("[^a-zA-Z0-9]",' ',$eq);
-		// echo $output;
 		$arr = explode(" ", $output);
 		return $arr;
 	}
@@ -65,15 +62,9 @@ class parse {
 		$query_array = $this -> get_func_list($query);
 		// get all functions available in php
 		$mathfunclist = $this -> math_func_list($query);
-		$c = 0;
 		foreach ($query_array as $key => $val) {
-			// echo "A $key == $val\n";
 			if ($val <> '' && $val <> ' ') {
 				if (!$this -> is_in_func_list($val)) {
-					$c++;
-					// I don't know what I'll do with this array
-					$exceptions[$c] = $val;
-					// echo "B $key == $val\n";
 					return TRUE;
 				}
 			}
@@ -85,16 +76,12 @@ class parse {
 	function eval_equation($query, $print_equation, $fun) {
 		try {
 			$xtest = $this -> get_func_list($query);
-			// echo "$xtest[1]\n";
 			// figure out that it is a calculus problem
-			// echo $query;
 			if ($this -> is_equation($query)) {
 				// evaluate the equation
 				$query_copy = str_replace('^', '**', $query);
 				$query_copy = str_replace('pi', '$pi', $query_copy);
 				$query_copy = str_replace('e', '$e', $query_copy);
-				// echo "$query_copy/n";
-				// echo number_format($e,4);
 				eval("
 				ini_set('display_errors', 0);
 				
@@ -174,7 +161,6 @@ mark1;
 			$parseObj = new parse;
 			// security check
 			$securetest = $parseObj -> func_test($_GET['q']);
-			// echo "Securetest: $securetest\n";
 			// true or false
 			if ($securetest == TRUE) {
 				echo 'error #3';
